9d1e64344318ffc8a9c6ada5ab1c6d9fecb50a28
BRYCE SMITH <a.bryce.smith.gmail.com>
2023-12-05 11:02:55 -0500

    fixed todo url and properties displayed.

--- demos/fetch-one-vs-many.js
+++ demos/fetch-one-vs-many.js
@@ -1,11 +1,18 @@
 fetch("http://jsonplaceholder.typicode.com/users")
-.then(response => response.json())
-.then(usersArray => {
+  .then((response) => response.json())
+  .then((usersArray) => {
     console.log(usersArray);
-});
+  });
 
 fetch("http://jsonplaceholder.typicode.com/users/5")
-.then(response => response.json())
-.then(userObject => {
+  .then((response) => response.json())
+  .then((userObject) => {
     console.log(userObject);
-});
+  });
+
+// //////todos
+// fetch("http://jsonplaceholder.typicode.com/todos/1")
+// .then(response => response.json())
+// .then(userObject => {
+//     console.log(userObject);
+// });
